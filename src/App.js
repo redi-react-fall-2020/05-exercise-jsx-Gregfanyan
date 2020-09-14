8c67c3b58f2f0d3af79177e9f6600520e69bd3d8
Gregfanyan <57916406+Gregfanyan@users.noreply.github.com>
2020-09-15 01:19:13 +0200

    done the assignment

--- src/App.js
+++ src/App.js
@@ -3,30 +3,40 @@ import "./styles.css";
 import logo from "./logo.svg";
 import team from "./team";
 
-/* 
-Use your new JSX skills to recreate our team chart.
-
-This app should have:
-- The same header as the original.
-- A table showing everyone's photo, name, and role (animals is a bonus).
-
-All of the imports have been provided for you, including:
-- Table data (team.js)
-- CSS (styles.css)
-- React logo (logo.svg)
-
-In your JSX, you'll need to use each of the following at least once...
-- className
-- variable
-- variable attribute
-- self-closing tag
-- map
-- key
-
-Feel free to have fun and experiment with others too!
-
-If you get stuck or need help, please reach out on Slack.
-*/
 export default function App() {
-  return <div>Replace this with your beautiful JSX</div>;
+  return (
+  
+    <>
+    <header className="App-header">
+      <img 
+        src={logo} 
+        alt="Spinning React logo" 
+        className="App-logo" 
+      />
+      <h1>Redi React Fall 2020 Team</h1>
+    </header>
+    <main>
+      <table className="members-table">
+        <thead>
+          <tr>
+            <th>Photo</th>
+            <th>Name</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {team.map((member) => (
+            <tr className="member-row" key={member.name}>
+              <td className="avatar-cell">
+                <img src={member.image} alt={member.name} />
+              </td>
+              <td className="name-cell">{member.name}</td>
+              <td className="role-cell">{member.role}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
+  </>
+  )
 }
